d93f2c37c80c12280dd6cc419d1452e210ba46a6
Advait Marathe <70425357+marathe-advait@users.noreply.github.com>
2021-05-24 20:06:02 +0530

    Updated Rand_Coor.py
    Now the coordinates are generated in the range of the Indian subcontinent.
    The Indian mainland extends from 8 degrees 4 minutes, = 8 + (4/60) = 8.06667 degrees, 
    to 37 degrees 6 minutes latitude.
    Similarly, in longitude from 68 degrees 7 minutes to 97 degrees 25 minutes.

--- Rand_Coor.py
+++ Rand_Coor.py
@@ -3,8 +3,8 @@ import random
 def rand_coor():
     pat_coor = [];
     loc_coor = [];
-    loc_coor.append(random.uniform(-180.000, 180.000))
-    loc_coor.append(random.uniform(-180.000, 180.000))
+    loc_coor.append(random.uniform(8.06667, 37.10000))
+    loc_coor.append(random.uniform(68.11667, 97.41667))
     pat_coor.append(loc_coor[0] + random.uniform(-2.500, 2.500))
     pat_coor.append(loc_coor[1] + random.uniform(-2.500, 2.500))
     return tuple(loc_coor), tuple(pat_coor)
